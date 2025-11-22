29f4f330b9f0eebb243ecc21cb5e213a5a6e37f7
weronika <weronikajedrzejewska@icloud.com>
2025-11-22 18:22:59 +0100

    Initial Commit

---
+++ rubik_solverTests/rubik_solverTests.swift
@@ -0,0 +1,17 @@
+//
+//  rubik_solverTests.swift
+//  rubik_solverTests
+//
+//  Created by Weronika on 22/11/2025.
+//
+
+import Testing
+@testable import rubik_solver
+
+struct rubik_solverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
